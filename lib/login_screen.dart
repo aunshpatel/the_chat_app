59fa268c9d93e9194e2643143253bd7546439114
aunshpatel <patelaunsh28@gmail.com>
2023-05-17 13:29:31 -0400

    Removed Model Progress HUD

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -5,7 +5,7 @@ import 'layouts.dart';
 import 'components/constants.dart';
 import 'components/rounded_buttons.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:modal_progress_hud/modal_progress_hud.dart';
+//import 'package:modal_progress_hud/modal_progress_hud.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -35,7 +35,7 @@ class _LoginScreenState extends State<LoginScreen> {
     return SafeArea(
         child: Scaffold(
             backgroundColor: darkTheme == false ? kLightBackgroundColor : kDarkBackgroundColor,
-            body: ModalProgressHUD(
+            /*body: ModalProgressHUD(
               inAsyncCall: showSpinner,
               child: Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 24.0),
@@ -162,7 +162,132 @@ class _LoginScreenState extends State<LoginScreen> {
                     ),
                   ),
               ),
-            )
+            )*/
+          body:Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height - 100,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Flexible(child: HeroLogo(tag:'logo',height: 250.0, image: 'images/the-chat-app-transparent.png'),),
+                  const SizedBox(
+                    height: 48.0,
+                  ),
+                  TextField(
+                    controller: emailController,
+                    keyboardType: TextInputType.emailAddress,
+                    onChanged:(value){
+                      email = emailController.text;
+                    },
+                    style: const TextStyle(color: kWhiteColor),
+                    decoration: emailInputDecoration('Enter your email'),
+                  ),
+                  const SizedBox(
+                    height: 15.0,
+                  ),
+                  TextField(
+                    controller: passwordController,
+                    obscureText: _passwordVisible == false ? true : false,
+                    onChanged:(value){
+                      pwd = passwordController.text;
+                    },
+                    style: const TextStyle(color: kWhiteColor),
+                    decoration: passwordInputDecoration(
+                        'Enter your password',
+                        _passwordVisible,
+                            (){
+                          setState(() {
+                            _passwordVisible = !_passwordVisible;
+                          });
+                        }
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 24.0,
+                  ),
+                  Row(
+                    children: [
+                      Checkbox(
+                        checkColor: kWhiteColor,
+                        side: MaterialStateBorderSide.resolveWith(
+                              (states) => const BorderSide(width: 1.0, color: kWhiteColor),
+                        ),
+                        fillColor: MaterialStateProperty.all(Colors.transparent),
+                        value: _isChecked,
+                        onChanged: (value){
+                          _isChecked = !_isChecked!;
+                          actionRemeberMe(_isChecked);
+                        },
+                      ),
+                      Text(
+                        'Remember Me',
+                        style: TextStyle(
+                            color: kWhiteColor,
+                            fontSize: 16
+                        ),
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 24.0,
+                  ),
+                  RoundedButton(
+                    colour:kLightBlueAccent,
+                    title:'Login',
+                    onPress:() async {
+                      if(emailController.text != '' && passwordController.text != '') {
+                        if(emailController.text.length < 6){
+                          _showMyDialog('Incorrect password! Please check your password length and try again.');
+                        }
+                        else{
+                          setState(() {
+                            showSpinner = true;
+                          });
+                          try{
+                            final user = await _auth.signInWithEmailAndPassword(email: emailController.text, password: passwordController.text);
+                            if(user != null){
+                              Navigator.pushNamed(context, ChatScreen.id);
+                              setState(() {
+                                showSpinner = false;
+                              });
+                            }
+                            else{
+                              _showMyDialog('Incorrect email or password. Please enter your email and password again.');
+                              setState(() {
+                                showSpinner = false;
+                              });
+                            }
+                          } catch(e){
+                            setState(() {
+                              showSpinner = false;
+                            });
+                            return _showMyDialog('${e.toString()}');
+                          }
+                        }
+                      }
+                      else if(passwordController.text.isEmpty && emailController.text.isNotEmpty){
+                        _showMyDialog('No password entered. Please enter the your password.');
+                      }
+                      else if(passwordController.text.isNotEmpty && emailController.text.isEmpty){
+                        _showMyDialog('No email id entered. Please enter your email id.');
+                      }
+                      else if(emailController.text.isEmpty && passwordController.text.isEmpty){
+                        _showMyDialog('Email and password fields are empty. Please enter both values to login.');
+                      }
+                      else if(emailController.text == '' && passwordController.text != ''){
+                        _showMyDialog('Please enter your email id to login.');
+                      }
+                      else if(emailController.text == '' && passwordController.text != ''){
+                        _showMyDialog('Please enter your password to login.');
+                      }
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
         )
     );
   }
--- lib/registration_screen.dart
+++ lib/registration_screen.dart
@@ -4,7 +4,7 @@ import 'components/rounded_buttons.dart';
 import 'components/constants.dart';
 import 'layouts.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:modal_progress_hud/modal_progress_hud.dart';
+//import 'package:modal_progress_hud/modal_progress_hud.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 final _registrationSctreenFirestore = FirebaseFirestore.instance;
@@ -28,7 +28,7 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
     return SafeArea(
       child: Scaffold(
           backgroundColor: darkTheme == false ? kLightBackgroundColor : kDarkBackgroundColor,
-          body: ModalProgressHUD(
+          /*body: ModalProgressHUD(
             inAsyncCall: showSpinner,
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 24.0),
@@ -165,8 +165,143 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                 ),
               ),
             ),
-          )
-        )
+          )*/
+        body:Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height-100,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Flexible(child: HeroLogo(tag:'logo',height: 250.0, image: 'images/the-chat-app-transparent.png'),),
+                const SizedBox(
+                  height: 48.0,
+                ),
+                TextField(
+                  onChanged:(value){
+                    fullName = value;
+                  },
+                  style: const TextStyle(color: kWhiteColor),
+                  decoration: emailInputDecoration('Enter your full name.'),
+                ),
+                const SizedBox(
+                  height: 15.0,
+                ),
+                TextField(
+                  onChanged:(value){
+                    emailID = value;
+                  },
+                  keyboardType: TextInputType.emailAddress,
+                  style: const TextStyle(color: kWhiteColor),
+                  decoration: emailInputDecoration('Enter your email'),
+                ),
+                const SizedBox(
+                  height: 15.0,
+                ),
+                TextField(
+                  obscureText: _passwordVisible == false ? true : false,
+                  onChanged:(value){
+                    pwd = value;
+                  },
+                  style: TextStyle(color: kWhiteColor),
+                  decoration: passwordInputDecoration(
+                      'Enter your password',
+                      _passwordVisible,
+                          (){
+                        setState(() {
+                          _passwordVisible = !_passwordVisible;
+                        });
+                      }
+                  ),
+                ),
+                const SizedBox(
+                  height: 15.0,
+                ),
+                TextField(
+                  obscureText: _confirmPasswordVisible == false ? true : false,
+                  onChanged:(value){
+                    confirmationPwd = value;
+                  },
+                  style: TextStyle(color: kWhiteColor),
+                  decoration: passwordInputDecoration(
+                      'Confirm your password',
+                      _confirmPasswordVisible,
+                          (){
+                        setState(() {
+                          _confirmPasswordVisible = !_confirmPasswordVisible;
+                        });
+                      }
+                  ),
+                ),
+                const SizedBox(
+                  height: 24.0,
+                ),
+                RoundedButton(
+                    colour:kBlueAccent,
+                    title:'Register',
+                    onPress:() async{
+                      if(emailID != '' && pwd != ''){
+                        if(pwd.length < 6){
+                          setState(() {
+                            showSpinner = false;
+                          });
+                          _showMyDialog('Please at least 6 characters for your password.');
+                        }
+                        else{
+                          if(confirmationPwd == pwd){
+                            setState(() {
+                              showSpinner = true;
+                            });
+                            try{
+                              final newUser = await _auth.createUserWithEmailAndPassword(email: emailID, password: pwd);
+                              if(newUser != null){
+                                _registrationSctreenFirestore.collection('registeredUsers').add({'fullName':fullName, 'emailID':emailID,});
+                                final user = await _auth.signInWithEmailAndPassword(email: emailID, password: pwd);
+                                if(user != null){
+                                  Navigator.pushNamed(context, ChatScreen.id);
+                                  setState(() {
+                                    showSpinner = false;
+                                  });
+                                }
+                                else{
+                                  _showMyDialog('Incorrect email or password. Please enter your email and password again.');
+                                }
+                              }
+                            } catch(e){
+                              setState(() {
+                                showSpinner = false;
+                              });
+                              return _showMyDialog('${e.toString()}');
+                            }
+                          }
+                          else {
+                            _showMyDialog('Your passwords are not same. Please check and enter again.');
+                          }
+                        }
+                      }
+                      else if(pwd.isEmpty && emailID.isNotEmpty){
+                        _showMyDialog('No password entered. Please enter the your password.');
+                      }
+                      else if(pwd.isNotEmpty && emailID.isEmpty){
+                        _showMyDialog('No email id entered. Please enter your email id.');
+                      }
+                      else if(emailID.isEmpty && pwd.isEmpty){
+                        _showMyDialog('Email and password fields are empty. Please enter both values to login.');
+                      }
+                      else if(emailID == '' && pwd != ''){
+                        _showMyDialog('Please enter your email id to login.');
+                      }
+                      else if(emailID == '' && pwd != ''){
+                        _showMyDialog('Please enter your password to login.');
+                      }
+                    }
+                ),
+              ],
+            ),
+          ),
+        ),
+      )
     );
   }
 
