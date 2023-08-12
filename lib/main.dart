2e4db1b1277d053004e102bec108ef6c2a8fc52b
aunshpatel <patelaunsh28@gmail.com>
2023-08-12 18:37:05 -0400

    Update firebase settings

--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,24 @@ import 'package:the_chat_app/registration_screen.dart';
 import 'package:the_chat_app/welcome_screen.dart';
 import 'chat_screen.dart';
 import 'login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async{
   runApp(const MyApp());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -30,3 +39,24 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       initialRoute: WelcomeScreen.id,
+//       routes: {
+//         WelcomeScreen.id : (context) => WelcomeScreen(),
+//         LoginScreen.id : (context) => LoginScreen(),
+//         RegistrationScreen.id : (context) => RegistrationScreen(),
+//         ChatScreen.id : (context) => ChatScreen(),
+//         ProfilePage.id : (context) => ProfilePage(),
+//         MainMenuPage.id : (context) => MainMenuPage(),
+//       },
+//     );
+//   }
+// }
